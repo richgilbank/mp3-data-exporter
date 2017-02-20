c0ad53613771cb8447cb47aea76a1e16ba9e274e
Rich Gilbank <richgilbank@gmail.com>
2017-02-20 11:36:38 -0500

    removing bin from git

--- docs/id3-to-csv-darwin-x64/id3-to-csv.app/Contents/Resources/app/promise-while.js
+++
@@ -1,9 +0,0 @@
-function promiseWhile(predicate, action) {
-  function loop() {
-    if (!predicate()) return;
-    return Promise.resolve(action()).then(loop);
-  }
-  return Promise.resolve().then(loop);
-}
-
-module.exports = promiseWhile
